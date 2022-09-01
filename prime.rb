84255fc18a9079eb61841c7a88f8a63642690240
cedricn8 <cedcn-9mail@yahoo.com>
2022-09-01 01:15:07 -0700

    commit tree 0fbb2fc997425245aff1ce23412dee52764957c0

--- prime.rb
+++ prime.rb
@@ -1,1 +1,33 @@
-# Add  code here!
+def prime?(number)
+    
+    flag = 1
+    i = 2
+    
+    if number < 0
+        n = number * -1
+    else
+        n = number
+    end
+    
+    if n > 1
+        while i <= n/2
+            if !(n % i == 0)
+                i += 1
+            else
+                flag = 0
+                break
+            end
+        end
+    end
+    if flag == 0 || n == 0 || n == 1
+        puts "#{number} is not prime"
+        false
+    else
+        puts "#{number} is prime"
+        true
+    end 
+end
+
+
+
+
